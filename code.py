4f20af2f71f928451ca18b0cedf2d96da88fd46f
hamdanbergi <hamdanalbergi16@gmail.com>
2026-04-29 13:00:07 +0530

    code.py

---
+++ code.py
@@ -0,0 +1,8 @@
+#BAND NAME GENERATOR
+print ("Hi there, let me find a name for your next Band")
+  
+city = input("What is the city you grew up in?\n") #first part in a two part band name 
+  
+pet = input("what pet do you have or had ?\n") + "s"  #second part in a two part band name
+
+print ("Your band name could be " + city + " " + pet ) # TWO PART BAND NAME BASED ON CITY AND PET
