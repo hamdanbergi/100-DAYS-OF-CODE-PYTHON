953521ac3998b908a5c5f5c68099dec0fe0f73c0
hamdanbergi <hamdanalbergi16@gmail.com>
2026-06-29 16:18:23 +0530

    MAZE_day_6.py

--- MAZE_day_6.py
+++ MAZE_day_6.py
@@ -1,1 +1,15 @@
-
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+def turn_around():
+    turn_left()
+    turn_left()
+while not at_goal():
+    if wall_in_front() and right_is_clear():
+        turn_right()
+    if front_is_clear():
+        move()
+    
+    if wall_on_right():
+        turn_left()
